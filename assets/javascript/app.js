9c7bdef063ebef92e5db6885c43c97ec79237f80
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-01-13 15:39:42 -0500

    added image to jumbotron, tweaked css, changed database values to be more presentable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,29 +10,26 @@ $(document).ready(function() {
     firebase.initializeApp(config);
 
   var database = firebase.database();
-  // var name = "";
-  // var destination = "";
-  // var firstTime = 0;
-  // var frequency = 0;
-  var nextTime = 0; //is that a string or a number becasue of time
-  var minutesAway = 0;
 
 
-
-	$("#add-train").on("click", function() {
+  $("#add-train").on("click", function() {
 	  var name = $("#name-input").val().trim();
 	  var destination = $("#destination-input").val().trim();
-      var firstTime = $("#time-input").val().trim();
+    var firstTime = $("#time-input").val().trim();
 	  var frequency = $("#frequency-input").val().trim();
 
 
-      database.ref().push({
+    database.ref().push({
         name: name,
         destination: destination,
         firstTime: firstTime,
         frequency: frequency
-      });
-
+    });
+      
+    $("#name-input").val("");
+    $("#destination-input").val("");
+	  $("#time-input").val("");
+	  $("#frequency-input").val("");
       
       return false;
     });
@@ -44,30 +41,32 @@ database.ref().on("child_added", function(snapshot){
 	console.log(snapshot.val().firstTime);
 	console.log(snapshot.val().frequency);
   
+  //redefining firstTime in snapshot
   var firstTime = snapshot.val().firstTime;
+  //redefining frequency in snapshot
   var frequency = snapshot.val().frequency;
-
   var currentTime = moment();
-      console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  console.log(currentTime);
 
   var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
 	  console.log(firstTimeConverted);
-
+  //difference in time between current time and converted time
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 	console.log("difference in time " + diffTime);
 
+  //determining remainder
   var tRemainder = diffTime % frequency;
 	console.log(tRemainder);
-
+  //from remainder determining the time away it is based on frequency
   var minutesAway = frequency - tRemainder;
 	console.log("minutes until train: " + minutesAway);
-
+  //calculating the next time based on the current time and minutesAway
   var nextTime = moment().add(minutesAway, "minutes");
 	console.log("arrival time: " + moment(nextTime).format("hh:mm"));
+  //made new variable for format
+  var nextTrainTime = moment(nextTime).format("hh:mm a");
 
-  var nextTrainTime = moment(nextTime).format("hh:mm");
-
-
+  	//creating table containing information in database and appending when new data created
 	$("#new-train").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination + "</td><td>" + frequency +  "</td><td>" + nextTrainTime +  "</td><td>" + minutesAway + "</td></tr>");
 
 	}, function(errorObject){
